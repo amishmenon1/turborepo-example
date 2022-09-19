23eff6ab40a3c5d1f855bf8c7d621fa3d4e67058
Amish Menon <amish.menon@allata.com>
2022-09-19 08:06:12 -0500

    fixed linting and testing

--- apps/admin/src/App.test.jsx
+++ apps/admin/src/App.test.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-describe('tests', () => {
-  it('should', () => {
+describe("test button rendering", () => {
+  it("should not be in document", () => {
     render(<App />);
-    const buttonElement = screen.findByText('Boop');
+    const buttonElement = screen.findByText("my test button");
+    waitFor(() => expect(buttonElement).not.toBeInTheDocument());
+  });
+  it("should be in document", () => {
+    render(<App />);
+    const buttonElement = screen.findByText("Boop");
     waitFor(() => expect(buttonElement).toBeInTheDocument());
   });
 });
